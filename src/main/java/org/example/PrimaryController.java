ff36505d4843ee106a222aef42821c2a1e9ee7dd
Workstation <simo.x@outlook.it>
2025-05-02 19:41:57 +0200

    Caricamento dell'imagine

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,7 +1,11 @@
 package org.example;
 
 import java.io.IOException;
+import java.util.Objects;
+
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
