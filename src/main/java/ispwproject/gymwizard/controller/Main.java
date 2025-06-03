557b41b4f1053dab82878e2806d573132e63bd3a
SimoX26 <simo.x@outlook.it>
2025-06-03 19:49:39 +0200

    HomeGUIController rewrite: Capture correct input from radius buttons

--- src/main/java/ispwproject/gymwizard/controller/Main.java
+++ src/main/java/ispwproject/gymwizard/controller/Main.java
@@ -1,4 +1,33 @@
 package ispwproject.gymwizard.controller;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/HomeView.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root); // Dimensione della finestra
+
+            primaryStage.setTitle("GymWizard");   // Titolo della finestra
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);     // Blocca ridimensionamento
+            primaryStage.centerOnScreen();        // Centra la finestra
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // e.getMessage();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/ispwproject/gymwizard/controller/gui/HomeGUIController.java
+++ src/main/java/ispwproject/gymwizard/controller/gui/HomeGUIController.java
@@ -2,10 +2,30 @@ package ispwproject.gymwizard.controller.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 
 public class HomeGUIController extends AbstractGUIController {
+
+    @FXML
+    private RadioButton select;
+
+    @FXML
+    private RadioButton radioInsegnante;
+
+    @FXML
+    private RadioButton radioContanti;
+
+    @FXML
+    private RadioButton radioCarta;
+
+    @FXML
+    private ToggleGroup version;
+
+    @FXML
+    private ToggleGroup memory;
 
     @FXML
     private AnchorPane anchorPane;
@@ -31,6 +51,21 @@ public class HomeGUIController extends AbstractGUIController {
 
     @FXML
     private void onLoginBtnClick(ActionEvent loginEvent) {
+        RadioButton selectVersion = (RadioButton) version.getSelectedToggle();
+        RadioButton selectMemory = (RadioButton) memory.getSelectedToggle();
+
+        if (selectVersion != null && selectMemory != null) {
+            String version = selectVersion.getText();
+            String memory = selectMemory.getText();
+
+            System.out.println("Versione: " + version + " - Memorizzazione: " + memory);
+
+            // Puoi anche passare questi valori al Model o Controller
+            // esempioController.salvaScelta(tipoUtente, tipoPagamento);
+        } else {
+            this.showError("Errore!", "Seleziona una scelta per entrambi i gruppi.");
+        }
+
         this.switchScene("/views/LoginView.fxml", loginEvent);
     }
 }
