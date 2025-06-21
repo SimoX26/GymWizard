891f51c7d102b97bc4809a5d8a31a8516942703a
simone <simo.x@outlook.it>
2025-06-21 12:15:46 +0200

    Sistemazione flusso attivita CLI - FINAL

--- src/main/java/ispwproject/gymwizard/controller/cli/AttivitaCLIController.java
+++ src/main/java/ispwproject/gymwizard/controller/cli/AttivitaCLIController.java
@@ -15,24 +15,22 @@ public class AttivitaCLIController {
     public void start() throws DAOException {
         List<Attivita> attivitaList = AttivitaController.getAttivitaDisponibili();
 
-        int i = 0;
         if(SessionManager.getInstance().getAttributo("homePage").equals("Admin")){
-            System.out.println("\n1. Crea nuova attività");
-            i++;
+            System.out.println("\nc. Crea nuova attività");
         }
+
         System.out.println("\n📌 ATTIVITÀ DISPONIBILI:\n");
-        for (; i < attivitaList.size(); i++) {
-            Attivita a = attivitaList.get(i);
-            System.out.println((i + 1) + ". " + a.getNome() + " | " + a.getData() + " | " + a.getOraInizio() + "-" + a.getOraFine() + " | posti disponibili: " + a.getPostiDisponibili());
+        for (int i = 0; i < attivitaList.size(); i++) {
+            System.out.println((i + 1) + ". " + attivitaList.get(i).getNome() + " | " + attivitaList.get(i).getData() + " | " + attivitaList.get(i).getOraInizio() + "-" + attivitaList.get(i).getOraFine() + " | posti disponibili: " + attivitaList.get(i).getPostiDisponibili());
         }
 
         System.out.println("\n0. 🔙 Torna alla Dashboard");
-        System.out.print("👉 Seleziona un'attività per info dettagliate (numero): ");
+        System.out.print("👉 Seleziona (numero): ");
         String scelta = scanner.nextLine();
 
         if ("0".equals(scelta)) {
             System.out.println("🔙 Ritorno alla dashboard cliente...");
-        } else if (SessionManager.getInstance().getAttributo("homePage").equals("Admin") && "1".equals(scelta)){
+        } else if (SessionManager.getInstance().getAttributo("homePage").equals("Admin") && "c".equals(scelta)){
             new CreaAttivitaCLIController().start();
         } else {
             Attivita a = attivitaList.get(Integer.parseInt(scelta) - 1);
--- src/main/java/ispwproject/gymwizard/controller/cli/RiepilogoAttivitaCLIController.java
+++ src/main/java/ispwproject/gymwizard/controller/cli/RiepilogoAttivitaCLIController.java
@@ -1,16 +1,26 @@
 package ispwproject.gymwizard.controller.cli;
 
+import ispwproject.gymwizard.controller.app.AttivitaController;
 import ispwproject.gymwizard.model.Attivita;
+import ispwproject.gymwizard.util.exception.DAOException;
 
 import java.util.Scanner;
 
 public class RiepilogoAttivitaCLIController {
     private final Scanner scanner = new Scanner(System.in);
 
-    public void start(Attivita a){
+    public void start(Attivita a) throws DAOException {
         System.out.println("\nAttività selezionata:\n");
-        System.out.println(a.getNome() + " | " + a.getData() + " | " + a.getOraInizio() + "-" + a.getOraFine() + " | posti disponibili: " + a.getPostiDisponibili());
-        System.out.print("👉 Premi invio per tornare: ");
-        scanner.nextLine();
+        System.out.println(a.getNome() + " | " + a.getData() + " | " + a.getOraInizio() + "-" + a.getOraFine() + " | posti disponibili: " + a.getPostiDisponibili() + " | nome trainer: " + a.getTrainerName());
+        System.out.print("\n1. Prenota un posto per l'attività ");
+        System.out.print("\n0. Tornare alla pagina precedente");
+        System.out.print("👉 : ");
+        String scelta = scanner.nextLine();
+
+        switch (scelta) {
+            case "1" -> AttivitaController.prenotaAttivita(a);
+            case "0" -> {System.out.println("Funzionalità non ancora implementata!");}
+            default -> System.out.println("❌ Scelta non valida.");
+        }
     }
 }
