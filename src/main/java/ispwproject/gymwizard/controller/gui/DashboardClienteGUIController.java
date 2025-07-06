9e21df1cd140e7827b214f3d37faeb0ba9eed13b
carum <chara.car02@gmail.com>
2025-07-06 10:09:21 +0200

    CS

--- src/main/java/ispwproject/gymwizard/controller/gui/DashboardClienteGUIController.java
+++ src/main/java/ispwproject/gymwizard/controller/gui/DashboardClienteGUIController.java
@@ -7,8 +7,12 @@ import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
+import java.util.logging.Logger;
 
 public class DashboardClienteGUIController extends AbstractGUIController{
+
+    private static final Logger logger = Logger.getLogger(DashboardClienteGUIController.class.getName());
+
     @FXML
     private Label welcomeLabel;
 
@@ -19,7 +23,7 @@ public class DashboardClienteGUIController extends AbstractGUIController{
     private void initialize() {
         anchorPane.setBackground(new Background(this.background()));
 
-        System.out.println("** INIT EXEC - Dashboard Client **");
+        logger.info("** INIT EXEC - Dashboard Client **");
         SessionManager.getInstance().setAttributo("homePage", "/views/DashboardClienteView.fxml");
 
         Utente u = (Utente) SessionManager.getInstance().getAttributo("utente");
@@ -28,43 +32,43 @@ public class DashboardClienteGUIController extends AbstractGUIController{
 
     @FXML
     public void onSchedaBtnClick(ActionEvent trainingCardEvent) {
-        System.out.println("SCHEDA ALLENAMENTO button clicked.");
+        logger.info("SCHEDA ALLENAMENTO button clicked.");
         this.switchScene("/views/VisualizzaSchedaView.fxml", trainingCardEvent);
     }
 
     @FXML
     public void onAttivitaBtnClick(ActionEvent coursesEvent) {
-        System.out.println("COURSES button clicked.");
+        logger.info("COURSES button clicked.");
         this.switchScene("/views/ListinoAttivitaView.fxml", coursesEvent);
     }
 
     @FXML
     public void onStatoAbbonamentoBtnClick(ActionEvent event) {
-        System.out.println("STATO ABBONAMENTO button clicked.");
+        logger.info("STATO ABBONAMENTO button clicked.");
         this.switchScene("/views/StatoAbbonamentoView.fxml", event);
     }
 
     @FXML
     public void onChatListBtnClick(ActionEvent event) {
-        System.out.println("CHAT button clicked.");
+        logger.info("CHAT button clicked.");
         this.switchScene("/views/ListaChatView.fxml", event);
     }
 
     @FXML
     public void onCodiceAccessoBtnClick(ActionEvent event) {
-        System.out.println("ACCESS CODE button clicked.");
+        logger.info("ACCESS CODE button clicked.");
         this.switchScene("/views/CodiceAccessoView.fxml", event);
     }
 
     @FXML
     public void onHelpClick() {
-        System.out.println("HELP button clicked.");
+        logger.info("HELP button clicked.");
         this.showPopup("Guida Interfaccia", "Dashboard Cliente", "Puoi accedere alle varie funzioni disponibili per un cliente della palestra.");
     }
 
     @FXML
     public void onLogoutClick(ActionEvent logoutEvent) {
-        System.out.println("LOGOUT button clicked.");
+        logger.info("LOGOUT button clicked.");
         if (this.logout()) {
             SessionManager.getInstance().clearAll();
             this.switchScene("/views/HomeView.fxml", logoutEvent);
