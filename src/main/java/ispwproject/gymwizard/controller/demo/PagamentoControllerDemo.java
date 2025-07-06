567986f169fdde9b713cb9d806a45b44064677ad
carum <chara.car02@gmail.com>
2025-07-06 09:37:45 +0200

    CS

--- src/main/java/ispwproject/gymwizard/controller/demo/PagamentoControllerDemo.java
+++ src/main/java/ispwproject/gymwizard/controller/demo/PagamentoControllerDemo.java
@@ -1,18 +1,28 @@
 package ispwproject.gymwizard.controller.demo;
 
 import ispwproject.gymwizard.controller.app.PagamentoController;
+import ispwproject.gymwizard.util.logger.AppLogger;
+
+import java.util.logging.Level;
 
 public class PagamentoControllerDemo extends PagamentoController {
 
     public PagamentoControllerDemo() {
         // Nessuna inizializzazione necessaria
-        System.out.println("[DEMO] PagamentoControllerDemo inizializzato (nessuna connessione PayPal).");
+        AppLogger.getLogger().log(
+                Level.INFO,
+                "[DEMO] PagamentoControllerDemo inizializzato (nessuna connessione PayPal)."
+        );
     }
 
     @Override
     public String creaOrdine(int prezzoCentesimi) {
         double prezzoEuro = prezzoCentesimi / 100.0;
-        System.out.println("[DEMO] Ordine creato per importo: €" + prezzoEuro);
+        AppLogger.getLogger().log(
+                Level.INFO,
+                "[DEMO] Ordine creato per importo: €{0}",
+                new Object[]{prezzoEuro}
+        );
 
         // Simula un URL PayPal mock
         return "https://demo.paypal.com/checkoutnow?ordine=demo123&amount=" + prezzoEuro;
