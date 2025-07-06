291f9150de1bd5f4cdb6b4f2ebc4eb729ce87af0
carum <chara.car02@gmail.com>
2025-07-06 10:19:12 +0200

    CS

--- src/main/java/ispwproject/gymwizard/controller/gui/ListaChatGUIController.java
+++ src/main/java/ispwproject/gymwizard/controller/gui/ListaChatGUIController.java
@@ -1,51 +1,59 @@
 package ispwproject.gymwizard.controller.gui;
 
+import ispwproject.gymwizard.util.logger.AppLogger;
 import ispwproject.gymwizard.util.singleton.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 
-public class ListaChatGUIController extends AbstractGUIController{
+import java.util.logging.Logger;
 
-    @FXML private VBox chatListVBox;
-
-    @FXML
-    AnchorPane anchorPane;
+public class ListaChatGUIController extends AbstractGUIController {
 
     @FXML
-    public void initialize(){
+    private VBox chatListVBox;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    private static final Logger logger = AppLogger.getLogger();
+
+    @FXML
+    public void initialize() {
         anchorPane.setBackground(new Background(this.background()));
     }
 
     @FXML
     public void onChatBtnClick(ActionEvent chatEvent) {
-        System.out.println("CHAT LIST button clicked.");
+        logger.info("CHAT LIST button clicked.");
         this.switchScene("/views/ChatView.fxml", chatEvent);
     }
 
     @FXML
     public void onBackClick(ActionEvent backEvent) {
-        System.out.println("BACK button clicked.");
+        logger.info("BACK button clicked.");
         this.switchScene((String) SessionManager.getInstance().getAttributo("homePage"), backEvent);
     }
 
     @FXML
     public void onHelpClick(ActionEvent event) {
-        System.out.println("HELP button clicked.");
+        logger.info("HELP button clicked.");
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Guida Interfaccia");
         alert.setHeaderText("Lista delle chat");
-        alert.setContentText("Puoi visualizzare la lista delle tue chat.\n" +
-                "Puoi scollare per scorrere la lista e scegliere la chat che vuoi aprire.");
+        alert.setContentText("""
+                Puoi visualizzare la lista delle tue chat.
+                Puoi scollare per scorrere la lista e scegliere la chat che vuoi aprire.
+                """);
         alert.showAndWait();
     }
 
     @FXML
     public void onHomeClick(ActionEvent homeEvent) {
-        System.out.println("HOME button clicked.");
+        logger.info("HOME button clicked.");
         this.switchScene((String) SessionManager.getInstance().getAttributo("homePage"), homeEvent);
     }
 }
