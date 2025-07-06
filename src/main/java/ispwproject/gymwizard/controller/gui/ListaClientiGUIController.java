9f3053af78ea5c04af92d1c5ace4166c1bafce94
carum <chara.car02@gmail.com>
2025-07-06 10:22:50 +0200

    CS

--- src/main/java/ispwproject/gymwizard/controller/gui/ListaClientiGUIController.java
+++ src/main/java/ispwproject/gymwizard/controller/gui/ListaClientiGUIController.java
@@ -3,6 +3,7 @@ package ispwproject.gymwizard.controller.gui;
 import ispwproject.gymwizard.controller.app.ClientiController;
 import ispwproject.gymwizard.controller.demo.DemoFactory;
 import ispwproject.gymwizard.model.Utente;
+import ispwproject.gymwizard.util.logger.AppLogger;
 import ispwproject.gymwizard.util.singleton.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,6 +14,7 @@ import javafx.scene.layout.VBox;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ListaClientiGUIController extends AbstractGUIController {
 
@@ -23,6 +25,7 @@ public class ListaClientiGUIController extends AbstractGUIController {
     private AnchorPane anchorPane;
 
     private final ClientiController clientiController = DemoFactory.getClientiController();
+    private static final Logger logger = AppLogger.getLogger();
 
     @FXML
     public void initialize() {
@@ -48,29 +51,30 @@ public class ListaClientiGUIController extends AbstractGUIController {
 
         } catch (SQLException e) {
             showError("Errore", "Errore durante il caricamento dei clienti: " + e.getMessage());
+            logger.severe("Errore durante il caricamento dei clienti: " + e.getMessage());
         }
     }
 
     @FXML
     public void onBackClick(ActionEvent backEvent) {
-        System.out.println("BACK button clicked.");
+        logger.info("BACK button clicked.");
         this.switchScene((String) SessionManager.getInstance().getAttributo("homePage"), backEvent);
     }
 
     @FXML
     public void onHelpClick() {
-        System.out.println("HELP button clicked.");
+        logger.info("HELP button clicked.");
         this.showPopup("Guida Interfaccia", "Lista Clienti", "Puoi visualizzare la lista dei clienti presenti nel sistema.");
     }
 
     @FXML
     public void onHomeClick(ActionEvent homeEvent) {
-        System.out.println("HOME button clicked.");
+        logger.info("HOME button clicked.");
         this.switchScene((String) SessionManager.getInstance().getAttributo("homePage"), homeEvent);
     }
 
     public void onClienteClick(ActionEvent event, Utente cliente) {
-        System.out.println("CLIENTE button clicked.");
+        logger.info("CLIENTE button clicked.");
         SessionManager.getInstance().setAttributo("clienteSelezionato", cliente);
         this.switchScene("/views/VisualizzaSchedaView.fxml", event);
     }
