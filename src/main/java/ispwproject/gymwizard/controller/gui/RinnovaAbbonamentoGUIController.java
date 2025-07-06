fb1922d596435323fd8a148ab2a38f4eae23bd60
carum <chara.car02@gmail.com>
2025-07-06 10:54:39 +0200

    CS

--- src/main/java/ispwproject/gymwizard/controller/gui/RinnovaAbbonamentoGUIController.java
+++ src/main/java/ispwproject/gymwizard/controller/gui/RinnovaAbbonamentoGUIController.java
@@ -1,12 +1,19 @@
 package ispwproject.gymwizard.controller.gui;
 
+import ispwproject.gymwizard.util.logger.AppLogger;
 import ispwproject.gymwizard.util.singleton.SessionManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 
+import java.util.logging.Logger;
+
 public class RinnovaAbbonamentoGUIController extends AbstractGUIController{
+
+    private static final Logger logger = AppLogger.getLogger();
+    private static final String TIPO_ABBONAMENTO = "tipoAbbonamento";
+    private static final String SCENE = "/views/RiepilogoOrdineView.fxml";
     @FXML
     AnchorPane anchorPane;
 
@@ -17,53 +24,53 @@ public class RinnovaAbbonamentoGUIController extends AbstractGUIController{
 
     @FXML
     private void on10IngressiClick(ActionEvent event) {
-        System.out.println("10 INGRESSI button clicked.");
-        SessionManager.getInstance().setAttributo("tipoAbbonamento", "10ingressi");
-        this.switchScene("/views/RiepilogoOrdineView.fxml", event);
+        logger.info("10 INGRESSI button clicked.");
+        SessionManager.getInstance().setAttributo(TIPO_ABBONAMENTO, "10ingressi");
+        this.switchScene(SCENE, event);
     }
 
     @FXML
     private void onMensileClick(ActionEvent event) {
-        System.out.println("MENSILE button clicked.");
-        SessionManager.getInstance().setAttributo("tipoAbbonamento", "mensile");
-        this.switchScene("/views/RiepilogoOrdineView.fxml", event);
+        logger.info("MENSILE button clicked.");
+        SessionManager.getInstance().setAttributo(TIPO_ABBONAMENTO, "mensile");
+        this.switchScene(SCENE, event);
     }
 
     @FXML
     private void onTrimestraleClick(ActionEvent event) {
-        System.out.println("TRIMESTRALE button clicked.");
-        SessionManager.getInstance().setAttributo("tipoAbbonamento", "trimestrale");
-        this.switchScene("/views/RiepilogoOrdineView.fxml", event);
+        logger.info("TRIMESTRALE button clicked.");
+        SessionManager.getInstance().setAttributo(TIPO_ABBONAMENTO, "trimestrale");
+        this.switchScene(SCENE, event);
     }
 
     @FXML
     private void onAnnualeClick(ActionEvent event) {
-        System.out.println("ANNUALE button clicked.");
-        SessionManager.getInstance().setAttributo("tipoAbbonamento", "annuale");
-        this.switchScene("/views/RiepilogoOrdineView.fxml", event);
+        logger.info("ANNUALE button clicked.");
+        SessionManager.getInstance().setAttributo(TIPO_ABBONAMENTO, "annuale");
+        this.switchScene(SCENE, event);
     }
 
     @FXML
     public void onBackClick(ActionEvent backEvent) {
-        System.out.println("BACK button clicked.");
+        logger.info("BACK button clicked.");
         switchScene("/views/StatoAbbonamentoView.fxml", backEvent);
     }
 
     @FXML
     public void onHelpClick(ActionEvent event) {
-        System.out.println("HELP button clicked.");
-        this.showPopup("Guida Interfaccia", "Rinnova Abbonamento", "\"\"\"\n" +
-                "                  Scegli una delle tipologie disponibili:\n" +
-                "                  - 10 Ingressi\n" +
-                "                  - Mensile\n" +
-                "                  - Trimestrale\n" +
-                "                  - Annuale\n" +
-                "                \"\"\"");
+        logger.info("HELP button clicked.");
+        this.showPopup("Guida Interfaccia", "Rinnova Abbonamento", """
+            Scegli una delle tipologie disponibili:
+            - 10 Ingressi
+            - Mensile
+            - Trimestrale
+            - Annuale
+            """);
     }
 
     @FXML
     public void onHomeClick(ActionEvent homeEvent) {
-        System.out.println("HOME button clicked.");
+        logger.info("HOME button clicked.");
         this.switchScene((String) SessionManager.getInstance().getAttributo("homePage"), homeEvent);
     }
 }
